c4e1e72b4f3a844dacced4a6380cb5ca942e271d
busrapinar <bsrapnrr@gmail.com>
2024-01-17 15:17:09 +0300

    axios, lodash, css edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AmountInput from "./components/AmountInput";
 import ResultRow from "./components/ResultRow";
+import axios from "axios";
+import {sortBy} from "lodash";
 
+
+type CachedResult = {
+  provider:string;
+  btc:string;
+}
 function App() {
   const [amount, setAmount] = useState("100");
+
+  const [cachedResults, setCachedResults] = useState<CachedResult[]>([]);
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get("https://27bxs3xs6n.us.aircode.run/cachedValues").then((res) => {
+      setCachedResults(res.data);
+      setLoading(false)
+    });
+  }, []);
+
+  const sortedCache = sortBy(cachedResults, "btc").reverse();
+
   return (
     <main className="max-w-2xl mx-auto px-4 py-8">
-      <h1 className="uppercase text-6xl text center 
+      <h1
+        className="uppercase text-6xl text center 
       font-bold bg-gradient-to-br from-purple-600 
-      to-sky-400 bg-clip-text text-transparent from-30%">
+      to-sky-400 bg-clip-text text-transparent from-30%"
+      >
         Find cheapest BTC
       </h1>
       <div className="flex justify-center mt-4 ">
-        <AmountInput 
-        value={amount} 
-        onChange={e => setAmount(e.target.value)}/>
+        <AmountInput
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
       </div>
       <div className="mt-6">
-        <ResultRow loading={true}/>
-        <ResultRow/>
-        <ResultRow/>
-        <ResultRow/>
+        {loading && (
+          <>
+            <ResultRow loading/>
+            <ResultRow loading/>
+            <ResultRow loading/>
+            <ResultRow loading/>
+          </>
+        )}
+        {!loading && sortedCache.map((result:CachedResult)=> (
+          <ResultRow providerName={result.provider} btc={result.btc}/>
+        ))}
       </div>
     </main>
   );
--- src/components/ResultRow.tsx
+++ src/components/ResultRow.tsx
@@ -1,26 +1,56 @@
-import React from 'react'
 
 type ResultRowProps = {
-loading? : boolean
-}
-const ResultRow = ({loading}:ResultRowProps) => {
-  return (
-    <div className='relative border border-white/10 min-h-12 rounded-lg 
-    bg-gradient-to-f from-purple-500/10 to-blue-500/10 p-4 my-2 overflow-hidden'>
-        <div className='flex gap-4'>
-            <div>logo</div>
-            <div className='grow'>provider name</div>
-            <div className='flex gap-2'>
-                <span className='text-xl text-purple-200/80'>0.000</span>
-                <span className='tet-xl text-purple-300/50'>BTC</span> 
-            </div>
-        </div>
-        {loading && (
-            <div className='inset-0 absolute bg-gradient-to-r from-transparent
-            via-blue-700/50 to-transparent skeleton-animation'/>
-        )}
-    </div>
-  )
-}
+  loading?: boolean;
+  providerName?: string;
+  btc?: string;
+};
 
-export default ResultRow
+type Logo = { source: string; invert?: boolean };
+
+const logos: { [keys: string]: Logo } = {
+  paybis: { source: 'https://raw.githubusercontent.com/dejwid/btc-table/master/src/assets/paybis.webp', invert: true },
+  guardian: { source: "https://guardarian.com/main-logo.svg" },
+  moonpay: { source: "https://www.moonpay.com/assets/logo-full-white.svg" },
+  transak: {
+    source: "https://assets.transak.com/images/website/transak-logo-white.svg",
+  },
+};
+const ResultRow = ({ loading, providerName, btc }: ResultRowProps) => {
+  return (
+    <div
+      className="relative border border-white/10 min-h-[64px] rounded-lg 
+    bg-gradient-to-f from-purple-500/10 to-blue-500/10 p-4 my-2 overflow-hidden"
+    >
+      <div className="flex gap-4">
+        {providerName && (
+          <div className="grow items-center flex">
+            <img
+              src={logos[providerName].source}
+              className={
+                "h-8 "+(logos[providerName]?.invert ? 'invert' : '')
+              }
+              alt=""/>
+          </div>
+        )}
+        {btc && (
+          <div className="flex gap-2">
+            <span className="text-xl text-purple-200/80">
+              {new Intl.NumberFormat("sv-SE", {
+                minimumFractionDigits: 8,
+              }).format(parseFloat(btc))}
+            </span>
+            <span className="tet-xl text-purple-300/50">BTC</span>
+          </div>
+        )}
+      </div>
+      {loading && (
+        <div
+          className="inset-0 absolute bg-gradient-to-r from-transparent
+            via-blue-700/50 to-transparent skeleton-animation"
+        />
+      )}
+    </div>
+  );
+};
+
+export default ResultRow;
